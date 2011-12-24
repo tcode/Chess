eec2035286abbce034d43d711c6dd8aaa713f972
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-24 18:01:58 +0100

    updates in comments

--- data.py
+++ data.py
@@ -7,11 +7,22 @@
 
 
 def main():
-        print "I am the data processing unit of this app!"
         fi = raw_input('please enter your desired file> ')
         f = open(fi, 'r') 
         data = f.read()
         print data
+
+        #to have a reprensentation of the moves you got to have a way to structure the board in memory.
+        #and while you do that, you might as well do bitboards that identifies wether a certain piece is at a certain 
+        #sqaure.
+
+        """
+        I would forinstance think that a board of 8x8 or 12x12 several times could be advantageus since then you have
+        to only write a lot less in terms of algorithms for figuring out where exactly the different pieces are
+
+        However there can be a certain pitfall in this as it might burn a lot of memory.
+        """
+
 
 
 
