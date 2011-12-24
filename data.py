e1e9ea0efb8a44cb8143f2ab9df3a43ce3fde507
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-24 17:40:46 +0100

    first code file commit

---
+++ data.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+#This is where all the heavy data processing power should be housed!
+#So this is to me the most critical part of this app.
+
+#This might be a particularly critical part of the product
