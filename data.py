8240d0b33c0e87d3c14c7f4d9d1c1af00a20e0b7
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-24 17:52:39 +0100

    First simple code

--- data.py
+++ data.py
@@ -4,3 +4,16 @@
 #So this is to me the most critical part of this app.
 
 #This might be a particularly critical part of the product
+
+
+def main():
+        print "I am the data processing unit of this app!"
+        fi = raw_input('please enter your desired file> ')
+        f = open(fi, 'r') 
+        data = f.read()
+        print data
+
+
+
+if __name__=='__main__':
+        main()
