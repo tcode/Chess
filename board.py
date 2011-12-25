c8c4f9d48c05af6b37a16dd87bfb576a6cc4b2ab
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 21:33:00 +0100

    more functions in board.py

--- board.py
+++ board.py
@@ -30,91 +30,156 @@ def edit_board(start, end, board):
     if start[1:] == 1:
         tmplist = board[1]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
                         
 
     if start[1:] == 2:
         tmplist = board[2]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
+
 
     if start[1:] == 3:
         tmplist = board[3]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     if start[1:] == 4:
         tmplist = board[4]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     if start[1:] == 5:
         tmplist = board[5]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     if start[1:] == 6:
         tmplist = board[6]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     if start[1:] == 7:
         tmplist = board[7]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     if start[1:] == 8:
         tmplist = board[8]
         if start[:1] == 'a':
+            tmplist[0] = None
         if start[:1] == 'b':
+            tmplist[0] = None
         if start[:1] == 'c':
+            tmplist[0] = None        
         if start[:1] == 'd':
+            tmplist[0] = None
         if start[:1] == 'e':
+            tmplist[0] = None
         if start[:1] == 'f':
+            tmplist[0] = None
         if start[:1] == 'g':
+            tmplist[0] = None
         if start[:1] == 'h':
+            tmplist[0] = None
 
     return board
 
