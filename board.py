fbd0a1c1429f74fce1f9b230636e875e4c89773c
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 14:26:10 +0100

    a little board representation code

---
+++ board.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#Here we are going to conceive of some chessboard reprensentation.
+# my first attempt would be a reprensentation
+
+def mlist(num):
+    new_list=[]
+    count = 0
+    while count < num:
+        new_list.append(0)
+        count = count + 1
+    print new_list
+
+
+mlist(8)
+
+   
