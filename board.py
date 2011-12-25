117be38c182feb82e25ec4b47937198b6d72839b
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 18:36:23 +0100

    A nicer board in board.py

--- board.py
+++ board.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 
+
+
+def print_board(board):
+    count = 0
+    lines = 8
+    while count < lines:
+        count=count+1
+        print board[count]
+
+def make_board(quad_size):
+    count = 0
+    board = {}
+    while count < quad_size:
+        count=count+1
+        board[count] = make_row(quad_size, count)
+    print_board(board)
+    
+
 def make_row(num, row_num):
     count = 0
     row = []
     while count < num:
         row.append(0)
         count=count+1
-    
     return row
 
 
-row1 = make_row(8, 1)
+make_board(8)
 
 
-print row1
+
+
+
+
 
