ba5fc962c337669262b93ba09ef723001cf32f28
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 17:52:01 +0100

    another approach

--- board.py
+++ board.py
@@ -1,38 +1,17 @@
 #!/usr/bin/env python
 
-#Here we are going to conceive of some chessboard reprensentation.
-# my first attempt would be a reprensentation
-
-def mlist(num):
-    new_list=[]
+def make_row(num, row_num):
     count = 0
+    row = []
     while count < num:
-        new_list.append(0)
-        count = count + 1
-    new_board(8, new_list)
-
-
-def new_board(num, blist):
-    count = 0
-    board = []
-    while count < num:
-        board.append(blist)
+        row.append(0)
         count=count+1
-    print_board(8, board)
-
-        #Technically this is a board of sorts.
-        #Now I just need to figure out how I do print it, but should be easy
-
-def print_board(num, board):
-    #Some thinking to be done here.
-    #Although it is not that difficult to write that print function
-    count = 0
-    while count < num:
-        print board[count]
-        count=count+1
+    
+    return row
 
 
+row1 = make_row(8, 1)
 
-mlist(8)
 
-   
+print row1
+
