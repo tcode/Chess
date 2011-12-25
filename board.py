b2662ac7ed41dcd664011db25ec1af026355e62a
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 14:34:25 +0100

    Update board.py

--- board.py
+++ board.py
@@ -10,7 +10,18 @@ def mlist(num):
         new_list.append(0)
         count = count + 1
     print new_list
+    new_board(8, new_list)
 
+
+def new_board(num, blist):
+    count = 0
+    board = []
+    while count < num:
+        board.append(blist)
+        count=count+1
+    
+        #Technically this is a board of sorts.
+        #Now I just need to figure out how I do print it, but should be easy
 
 mlist(8)
 
