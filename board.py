51047a1561a6b950288982f80584e6c385816770
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-25 16:35:06 +0100

    board works, as it represents the board as 8x8 with no content

--- board.py
+++ board.py
@@ -18,9 +18,20 @@ def new_board(num, blist):
     while count < num:
         board.append(blist)
         count=count+1
-    
+    print_board(8, board)
+
         #Technically this is a board of sorts.
         #Now I just need to figure out how I do print it, but should be easy
+
+def print_board(num, board):
+    #Some thinking to be done here.
+    #Although it is not that difficult to write that print function
+    count = 0
+    while count < num:
+        print board[count]
+        count=count+1
+
+
 
 mlist(8)
 
