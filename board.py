c8d2104d5f41168236b3121431f87b2e25caa430
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-29 01:12:21 +0100

    board is a bit better now, began to add support for different pieces movement, to make the later development a little easier

--- board.py
+++ board.py
@@ -9,8 +9,9 @@ def print_board(board):
         count=count+1
         print board[count]
 
-def make_board(quad_size):
+def make_board():
     count = 0
+    quad_size=8
     board = {}
     while count < quad_size:
         count=count+1
@@ -55,10 +56,33 @@ def edit_board():
 #and on the 3rd row, and knight jumps and all on the diagonals. (However when something 
 # stands in the way it should stop to relate, but that is an inprogram problem).
 
-nboard = make_board(8)
+nboard = make_board()
 print_board(nboard)
 
 
-
-
+def bishop_move(board):
+    row = input('What row do you want ? ')
+    line = raw_input('What line do you want ? ')
+    # Some row and file
+    # Now for some evaluation of this information to retrieve where the pieces are.
+    current_row = board[row]
+    #The nasty part here is that I want to check for both the text and number representation
+    if str(line) == 'a' or int(line) == 1:
+        print current_row[1]
+    if str(line) == 'b' or int(line) == 2:
+         print current_row[2]
+    if str(line) == 'c' or int(line) == 3:
+        print current_row[3]
+    if str(line) == 'd' or int(line) == 4:
+        print current_row[4] 
+    if str(line) == 'e' or int(line) == 5:
+        print current_row[5]
+    if str(line) == 'f' or int(line) == 6:
+        print current_row[6]
+    if str(line) == 'g' or int(line) == 7:
+        print current_row[7]
+    if str(line) == 'h' or int(line) == 8:
+        print current_row[8]
+#The above is great there is a minimal of if statements so it is not so cluttered.
+#However I have not began to define any movement yet, but I am to tired to do it now.
 
