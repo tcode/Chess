3ed98a1fc35f454a0309056da7b2d1ce8dfa0ef5
Tobias Lindgaard <tobias.private@gmail.com>
2012-01-02 23:24:02 +0100

    Now the basics of moving pieces are implemented

--- move.py
+++ move.py
@@ -8,6 +8,37 @@ from board import *
 
 board = make_board()
 
+
+
+
 def extract_row(num):
     row = board[num]
+    return row
+
+def return_row(num, row):
+    board[num] = row
+
+# crow = extract_row(10)
+# crow[6] = '--'
+# crow = extract_row(8)
+# crow[5] = 'WN'
+
+def move(srow, sline, erow, eline):
+    working = extract_row(srow)
+    tmp = working[sline]
+    working[sline] = '--'
+    end = extract_row(erow)
+    end[eline] = str(tmp)
+
+# The above could should be a function, but with how many elements ? 4 2 ?
+# with 4 there is a lot of parameters to put in, but perhaps it is a little easier to make it automatic 
+# and thus make it easier to make the program work a little better, 2 variables would be nicer if it 
+# involves direct human interaction with this part of the package. - well even if it is designed for
+# automatically generating appropriate data from part to part, it can still work with human 
+# interaction. That could be an important part of the program later.
+#move(3-10, 0-7, 3-10, 0-7)
+move(9, 4, 7, 4)
+
+print_board(board)
+
 
