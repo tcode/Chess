453136ff4283e5ca8019d6721bff0666438a88bd
Tobias Lindgaard <tobias.private@gmail.com>
2012-01-02 16:05:13 +0100

    removed some bad code

--- move.py
+++ move.py
@@ -3,3 +3,11 @@
 # Copyright Tobias Lindgaard 2012, part of ChessPath
 # Distributed under the GNU GPL
 # For more information about the License see LICENSE.
+
+from board import *
+
+board = make_board()
+
+def extract_row(num):
+    row = board[num]
+
