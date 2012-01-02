ae0e59f45ed5ee26c8fa1ddd172d94102bf18d72
Tobias Lindgaard <tobias.private@gmail.com>
2012-01-02 23:33:35 +0100

    edited a little in convert.py

--- convert.py
+++ convert.py
@@ -7,3 +7,20 @@ def import_pgn():
     fil = open(pushed_file, 'r')
     content = fil.read()
     
+
+
+
+def extract_info(fi):
+    pgn = open(fi, 'r')
+    cont = pgn.read()
+    event = re.search(r'.Event.+', cont)
+    site = re.search(r'.Site.+', cont)
+    date = re.search(r'.Date.+', cont)
+    mround = re.search(r'.Round.+', cont)
+    white = re.search(r'.White.+', cont)
+    black = re.search(r'.Black.+', cont)
+    result = re.search(r'.Result.+', cont)
+    moves = re.search(r'1\.\s.+(1|0|1/2)', cont)
+
+
+
